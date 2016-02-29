7b5864a86a466f95c994a74aa1c23252e59117d0
Sarah Wells <sarah.wells@ft.com>
2016-02-29 14:49:10 +0000

    First pass at read query

--- cypher.go
+++ cypher.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/jmcvetta/neoism"
 )
@@ -21,7 +23,7 @@ func newCypherDriver(db *neoism.Database, env string) cypherDriver {
 	return cypherDriver{db, env}
 }
 
-func (pcw cypherDriver) checkConnectivity() error {
+func (cd cypherDriver) checkConnectivity() error { //TODO - use the neo4j connectivity check library
 	results := []struct {
 		ID int
 	}{}
@@ -29,7 +31,7 @@ func (pcw cypherDriver) checkConnectivity() error {
 		Statement: "MATCH (x) RETURN ID(x) LIMIT 1",
 		Result:    &results,
 	}
-	err := pcw.db.Cypher(query)
+	err := cd.db.Cypher(query)
 	log.Debugf("CheckConnectivity results:%+v  err: %+v", results, err)
 	return err
 }
@@ -37,7 +39,34 @@ func (pcw cypherDriver) checkConnectivity() error {
 type neoReadStruct struct {
 }
 
-func (pcw cypherDriver) read(uuid string) (anns annotations, found bool, err error) {
+func (cd cypherDriver) read(contentUUID string) (anns annotations, found bool, err error) {
 	results := []annotation{}
+
+	query := &neoism.CypherQuery{
+		Statement: `
+					MATCH (c:Thing{uuid:{contentUUID}})-[rel]->(cc:Thing)
+					RETURN cc.uuid as id,
+					type(rel) as predicate,
+					labels(cc) as types,
+					cc.prefLabel as prefLabel,
+					cc.leiCode as leiCode
+					`,
+		Parameters: neoism.Props{"contentUUID": contentUUID},
+		Result:     &results,
+	}
+	err = cd.db.Cypher(query)
+	if err != nil {
+		log.Errorf("Error looking up uuid %s with query %s from neoism: %+v", contentUUID, query.Statement, err)
+		return annotations{}, false, fmt.Errorf("Error accessing Annotations datastore for uuid: %s", contentUUID)
+	}
+	log.Debugf("CypherResult Read Annotations for uuid: %s was: %+v", contentUUID, results)
+	if (len(results)) == 0 {
+		return annotations{}, false, nil
+	}
+
+	// for idx := range results {
+	// 	mapToResponseFormat(&results[idx])
+	// }
+
 	return results, true, nil
 }
