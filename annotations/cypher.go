6ed7906ea9c8e2149b811f7b0eafad680b74a4f8
Clark, Peter <peter.clark@ft.com>
2017-11-27 11:55:04 +0000

    :art: Adding uuid

--- annotations/cypher.go
+++ annotations/cypher.go
@@ -94,7 +94,7 @@ func (cd cypherDriver) read(contentUUID string) (anns annotations, found bool, e
 	err = cd.conn.CypherBatch([]*neoism.CypherQuery{query})
 	end := time.Now()
 
-	log.WithField("duration", fmt.Sprintf("%vms", end.Sub(start).Nanoseconds()/1e6)).Info("Annotations query (including implicit relationships) completed.")
+	log.WithField("contentUUID", contentUUID).WithField("duration", fmt.Sprintf("%vms", end.Sub(start).Nanoseconds()/1e6)).Info("Annotations query (including implicit relationships) completed.")
 
 	if err != nil {
 		log.Errorf("Error looking up uuid %s with query %s from neoism: %+v", contentUUID, query.Statement, err)
